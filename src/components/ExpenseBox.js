af531aa473019c0f2ffe5affbaa0897ae693e0e5
Nishan Bhandari <bnishan18@gmail.com>
2023-03-05 17:01:23 +0545

    bug fixed

--- src/components/ExpenseBox.js
+++ src/components/ExpenseBox.js
@@ -1,4 +1,3 @@
-import { async } from "@firebase/util";
 import {
   Dialog,
   DialogContent,
@@ -7,30 +6,129 @@ import {
   DialogActions,
   Button,
   Typography,
+  Snackbar,
+  Alert,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
-
+import { getAuth } from "firebase/auth";
 import { uploadImage } from "../Firebaseconfig/storage";
 
 const ExpenseBox = ({ onClose }) => {
   const [formData, setFormData] = useState({});
   const [open, setOpen] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const [files, setFiles] = useState(null);
 
-  const handleSubmit = async (event) => {
+  const authUser = getAuth();
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     await uploadImage(formData.file);
+  //     console.log("success");
+  //     onClose();
+  //     setFormData({});
+  //     setOpen(true)
+  //     // <Snackbar open={open} autoHideDuration={5000}>
+  //     //   <Alert severity="success" sx={{ width: "100%" }}>
+  //     //     Expense Added !{" "}
+  //     //   </Alert>
+  //     // </Snackbar>;
+  //   } catch (error) {
+  //     console.log("Unsuccess");
+  //     setOpen(true)
+
+  //     // <Snackbar open={open} autoHideDuration={5000}>
+  //     //   <Alert severity="error" sx={{ width: "100%" }}>
+  //     //     Error please try again !
+  //     //   </Alert>
+  //     // </Snackbar>;
+  //   }
+  // };
+
+  //  if (files) {
+  //   setFormData((prevFormData) =>({
+  //     ...prevFormData,
+  //     file : files[0],
+  //   }));
+
+  //   const reader = new FileReader();
+  //   reader.onload = (e) =>{
+  //     setImageUrl(e.target.result);
+  //   };
+  //   reader.onload = (e) =>{
+  //     setImageUrl(e.target.result);
+  //   };
+  //   reader.readAsDataURL(files[0]);
+
+  // }else{
+  //   setFormData((prevFormData)=>({...prevFormData,[name]:value}));
+  // }
+
+  const handleSubmit = async (event, name, value) => {
     event.preventDefault();
-    // onClose();
     try {
-      await uploadImage(formData.file);
-      console.log("success");
+      if (files) {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          file: files[0],
+        }));
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          setImageUrl(e.target.result);
+        };
+        reader.readAsDataURL(files[0]);
+      } else {
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+        // <Snackbar open={open} autoHideDuration={5000}>
+        //   <Alert severity="error" sx={{ width: "100%" }}>
+        //     Error please try again !
+        //   </Alert>
+        // </Snackbar>;
+      }
+      console.log("successful");
     } catch (error) {
       console.log("Unsuccess");
+      setOpen(true);
+
+      // <Snackbar open={open} autoHideDuration={5000}>
+      //   <Alert severity="error" sx={{ width: "100%" }}>
+      //     Error please try again !
+      //   </Alert>
+      // </Snackbar>;
     }
+    handleClose();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    onClose();
+    setFormData({});
   };
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    const { name, value, files } = event.target;
+    // setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+
+    if (files) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        file: files[0],
+      }));
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setImageUrl(e.target.result);
+      };
+      reader.onload = (e) => {
+        setImageUrl(e.target.result);
+      };
+      reader.readAsDataURL(files[0]);
+    } else {
+      setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    }
   };
   return (
     <>
@@ -43,6 +141,13 @@ const ExpenseBox = ({ onClose }) => {
       </Box>
       <Dialog open={open} onClose={onClose}>
         <DialogTitle>ADD EXPENSES</DialogTitle>
+        <DialogContent
+          sx={{ display: "flex", alignItems: "center", gap: "20px" }}
+        >
+          {imageUrl && <img src={imageUrl} alt="Selected" height={100} />}
+          <input type="file" onChange={handleInputChange}></input>
+        </DialogContent>
+
         <DialogContent>
           <TextField
             autoFocus
@@ -93,10 +198,16 @@ const ExpenseBox = ({ onClose }) => {
           ></TextField>
         </DialogContent>
         <DialogActions>
-          <Button onClick={onClose}>Close</Button>
+          <Button onClick={handleClose}>Close</Button>
           <Button onClick={handleSubmit}>Submit</Button>
         </DialogActions>
+    
       </Dialog>
+      <Snackbar open={open} autoHideDuration={5000}>
+      <Alert severity="success" sx={{ width: "100%" }}>
+        Expense Added !{" "}
+      </Alert>
+    </Snackbar>;
     </>
   );
 };
